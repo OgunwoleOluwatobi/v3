2a52ea0f382755bae6391d18b813de9a07d7f350
ogunwoleoluwatobi <oluwatstar21@gmail.com>
2020-05-10 02:17:48 +0100

    Quick Fix

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -11,7 +11,20 @@ const GlobalStyle = createGlobalStyle`
       box-sizing: border-box;
       width: 100%;
   }
-  
+  body {
+    margin: 0;
+    width: 100%;
+    min-height: 100%;
+    overflow-x: hidden;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    background-color: #14181E;
+    font-family: ${fonts.Calibre};
+    line-height: 1.3;
+    &.hidden {
+      overflow: hidden;
+    }
+  }
   a {
     text-decoration: none;
     color: inherit;
